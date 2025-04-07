b02e08b0f0a68892b077f1c0c63f538d33a8422c
shivgitcode <shivneeraj2004@gmail.com>
2025-04-07 12:27:10 +0530

    fourth commit

--- index.ts
+++ index.ts
@@ -8,6 +8,12 @@ app.get("/", (req, res) => {
 	});
 });
 
+app.get("/hello", (req, res) => {
+	res.status(200).json({
+		message: "hello",
+	});
+});
+
 app.listen(5000, () => {
 	console.log("Server running on port 5000");
 });
